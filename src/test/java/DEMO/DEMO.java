6aeeead6f0c4f70f2df99b994049c94826a58197
shilpa <shilpa.ms1018@gmail.com>
2024-03-13 10:50:31 +0530

    THIRD

--- src/test/java/DEMO/DEMO.java
+++ src/test/java/DEMO/DEMO.java
@@ -1,11 +1,19 @@
 package DEMO;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class DEMO {
 	
-	public static void main(String args[]) {
+	@Test
+	public void api() {
 		
-		System.out.println("DEMO1");
-		System.out.println("added develop branch1");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com/");
+//		System.out.println("DEMO1");
+//		System.out.println("added develop branch1");
+		
 	}
 
 }
